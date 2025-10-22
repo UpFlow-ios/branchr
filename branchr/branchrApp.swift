18fd7c64b806487d14522c2b64cbe850bc9b154f
Joe Dormond <upflowapp@gmail.com>
2025-10-21 17:44:20 -0700

    Initial Commit

---
+++ branchr/branchrApp.swift
@@ -0,0 +1,17 @@
+//
+//  branchrApp.swift
+//  branchr
+//
+//  Created by Joe Dormond on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct branchrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ branchrTests/branchrTests.swift
@@ -0,0 +1,16 @@
+//
+//  branchrTests.swift
+//  branchrTests
+//
+//  Created by Joe Dormond on 10/21/25.
+//
+
+import Testing
+
+struct branchrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
